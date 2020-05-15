91a0eb4cdfbd043051c892eb2bf9928f8d896825
lemontree2000 <gonelove.guo@gmail.com>
2020-05-15 23:04:05 +0800

    feat(core/interceptorManager.ts): add interceptor

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,7 +33,7 @@ export interface AxiosResponse<T = any> {
   request: any
 }
 
-export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> { }
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 export interface AxiosError extends Error {
   isAxiosError: boolean
@@ -55,6 +55,19 @@ export interface Axios {
 }
 
 export interface AxiosInstance extends Axios {
-  <T=any>(config: AxiosRequestConfig): AxiosPromise<T>
-  <T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected: RejectedFn): number
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
 }
