bcaaab969777656a3eb6e9f8fe33f3059f89d1f7
lemontree2000 <gonelove.guo@gmail.com>
2020-06-15 00:06:31 +0800

    test(helper/util.ts): test util.ts

---
+++ test/helpers/util.spec.ts
@@ -0,0 +1,114 @@
+import {
+  isDate,
+  isFormData,
+  isObject,
+  isPlainObject,
+  isURLSearchParams,
+  extend,
+  deepMerge
+} from '../../src/helpers/util'
+
+describe('helpers:util', () => {
+  describe('isXX', () => {
+    test('should validate Date', () => {
+      expect(isDate(new Date())).toBeTruthy()
+      expect(isDate(Date.now())).toBeFalsy()
+    })
+
+    test('should validate FormData', () => {
+      expect(isFormData(new FormData())).toBeTruthy()
+      expect(isFormData('FormDate')).toBeFalsy()
+    })
+
+    test('should validate isObject', () => {
+      expect(isObject([1, 2, 3, 4])).toBeTruthy()
+      expect(isObject(new Date())).toBeTruthy()
+      expect(isObject(1234)).toBeFalsy()
+    })
+
+    test('should validate isPlainObject', () => {
+      expect(isPlainObject({ text: 'hello object' })).toBeTruthy()
+      expect(isPlainObject(new Date())).toBeFalsy()
+    })
+
+    test('should validate isURLSearchParams', () => {
+      expect(isURLSearchParams(new URLSearchParams())).toBeTruthy()
+      expect(isURLSearchParams('a=b&c=a')).toBeFalsy()
+    })
+  })
+
+  describe('extend', () => {
+    test('should be mutable', () => {
+      const a = Object.create(null)
+      const b = { foo: 1 }
+      extend(a, b)
+      expect(a.foo).toBe(1)
+    })
+
+    test('should extend properties', () => {
+      const a = { foo: 123, bar: 456 }
+      const b = { bar: 789 }
+      const c = extend(a, b)
+      expect(c.bar).toBe(789)
+      expect(c.foo).toBe(123)
+    })
+  })
+
+  describe('deepMerge', () => {
+    test('should be immutable', () => {
+      const a = Object.create(null)
+      const b: any = { foo: 123 }
+      const c: any = { bar: 456 }
+
+      deepMerge(a, b, c)
+
+      expect(a.foo).toBeUndefined()
+      expect(a.bar).toBeUndefined()
+      expect(b.bar).toBeUndefined()
+      expect(c.foo).toBeUndefined()
+    })
+
+    test('should deepMerge properties', () => {
+      const a = { foo: 123 }
+      const b = { bar: 456 }
+      const c = { foo: 789 }
+      const d = deepMerge(a, b, c)
+
+      expect(d.foo).toBe(789)
+      expect(d.bar).toBe(456)
+    })
+
+    test('should deepMerge recursively', () => {
+      const a = { foo: { bar: 123 } }
+      const b = { foo: { baz: 456 }, bar: { qux: 789 } }
+      const c = deepMerge(a, b)
+
+      expect(c).toEqual({
+        foo: { bar: 123, baz: 456 },
+        bar: { qux: 789 }
+      })
+    })
+
+    test('should remove all references from nested objects', () => {
+      const a = { foo: { bar: 123 } }
+      const b = {}
+      const c = deepMerge(a, b)
+
+      expect(c).toEqual({
+        foo: {
+          bar: 123
+        }
+      })
+      expect(c.foo).not.toBe(a.foo)
+    })
+
+    test('should handle null and undefined arguments', () => {
+      expect(deepMerge(undefined, undefined)).toEqual({})
+      expect(deepMerge(undefined, { foo: 123 })).toEqual({ foo: 123 })
+      expect(deepMerge({ foo: 123 }, undefined)).toEqual({ foo: 123 })
+      expect(deepMerge(null, null)).toEqual({})
+      expect(deepMerge(null, { foo: 123 })).toEqual({ foo: 123 })
+      expect(deepMerge({ foo: 123 }, null)).toEqual({ foo: 123 })
+    })
+  })
+})
